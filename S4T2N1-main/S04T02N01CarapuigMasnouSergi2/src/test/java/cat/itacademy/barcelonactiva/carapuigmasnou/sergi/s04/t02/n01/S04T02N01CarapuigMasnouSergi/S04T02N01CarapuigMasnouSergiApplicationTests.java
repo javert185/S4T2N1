e5f493b2c30599d2a5496b4bbe8777a25f865945
javert185 <124713607+javert185@users.noreply.github.com>
2023-08-11 11:43:00 +0200

    Add files via upload

---
+++ S4T2N1-main/S04T02N01CarapuigMasnouSergi2/src/test/java/cat/itacademy/barcelonactiva/carapuigmasnou/sergi/s04/t02/n01/S04T02N01CarapuigMasnouSergi/S04T02N01CarapuigMasnouSergiApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.carapuigmasnou.sergi.s04.t02.n01.S04T02N01CarapuigMasnouSergi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01CarapuigMasnouSergiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
